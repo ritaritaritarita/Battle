5b5aca6ccc54fa25de398ed78598fe3a6e174d28
Ebube Ud <kripsonud@gmail.com>
2021-08-06 09:36:28 +0100

    Fix issue request function in RandomNumberGenerator

--- contracts/RandomNumberGenerator.sol
+++ contracts/RandomNumberGenerator.sol
@@ -34,7 +34,7 @@ contract RandomNumberGenerator is VRFConsumerBase {
      */
     function request(uint256 userProvidedSeed) public returns (bytes32 requestId) {
         require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
-        return requestRandomness(keyHash, fee);
+        return requestRandomness(keyHash, fee, userProvidedSeed);
     }
 
     /**
